f10cba70821d4f0c421d7b93b75649a75e36d62f
Eric Farr <eric.farr@clear-measure.com>
2018-10-20 10:01:20 -0400

    Basic project tree in Markdown

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace find_projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string parentDir = args.Length > 0 ? args[0] : Directory.GetCurrentDirectory();
+            ProjectFinder finder = new ProjectFinder(parentDir);
+
+            Node root = finder.GetProjectTree();
+            foreach (Node node in root.Children.Values)
+            {
+                PrintLine(node, 1);
+            }
+        }
+
+        // Print with Markdown formatting
+        static void PrintLine(Node node, int indent)
+        {
+            if (node.IsLeaf())
+                Console.WriteLine($"- {node.Name}");
+            else
+            {
+                Console.WriteLine($"\r\n{new string('#', indent)} {node.Name}");
+                foreach (Node child in node.Children.Values)
+                {
+                    PrintLine(child, indent+1);
+                }
+            }
+        }
+    }
+}
---
+++ ProjectFinder.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace find_projects
+{
+    public class Node
+    {
+        public Node(string name)
+        {
+            Name = name;
+            Children = new Dictionary<string, Node>();
+        }
+
+        public Node AddChild(string name)
+        {
+            if (!Children.ContainsKey(name))
+                Children[name] = new Node(name);
+
+            return Children[name];
+        }
+
+        public bool IsLeaf()
+        {
+            if (Children.Count > 1)
+                return false;
+            if (Children.Count == 0)
+                return true;
+
+            Node onlyChild = Children.First().Value;
+
+            if (onlyChild.Children.Count == 0)
+                return true;
+
+            if (onlyChild.Children.Count > 1)
+                return false;
+
+            if (onlyChild.Children.Values.First().Children.Count > 0)
+                return false;
+
+            // If my only child name matches my name, it's most likely just superfluous nesting 
+            return String.Equals(Name, onlyChild.Name, StringComparison.CurrentCultureIgnoreCase);
+        }
+
+        public string Name { get; set; }
+        public Dictionary<string,Node> Children { get; set; }
+    }
+
+    public class ProjectFinder
+    {
+        private readonly string _parent;
+        readonly List<string> _files = new List<string>();
+
+        public ProjectFinder(string parent)
+        {
+            _parent = parent;
+        }
+
+        private void GetFilesRecursive(string dirPath)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(dirPath))
+                {
+                    GetFilesRecursive(d);
+                }
+                foreach (var file in Directory.GetFiles(dirPath))
+                {
+                    if (IsProjectFile(file))
+                        ProcessFile(file);
+                }
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private bool IsProjectFile(string file)
+        {
+            return file.ToLower().EndsWith(".csproj");
+        }
+
+        private void ProcessFile(string file)
+        {
+            _files.Add(file.Substring(_parent.Length));
+        }
+
+        public Node GetProjectTree()
+        {
+            GetFilesRecursive(_parent);
+            Node root = new Node("root");
+
+            foreach (string file in _files)
+            {
+                Stack<string> stack = new Stack<string>(file.Split('\\').Reverse());
+                AddFile(root, stack);
+            }
+
+            return root;
+        }
+
+        private void AddFile(Node parent, Stack<string> stack)
+        {
+            string name = stack.Pop();
+            Node node = parent.AddChild(name);
+            if (stack.Count > 0)
+                AddFile(node, stack);
+        }
+    }
+}
